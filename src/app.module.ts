903ec592c8523555d683b2b5ae549c756c6234df
Ramsha-Anwar <ramshaanwar01@gmail.com>
2026-08-15 00:01:15 +0500

    Create base NestJS project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from "@nestjs/config";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import { Note } from "./notes/notes.entity";
-import { NotesModule } from "./notes/notes.module";
 
 @Module({
-  imports: [
-     ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    NotesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.POSTGRES_URL,
-      entities: [Note],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
